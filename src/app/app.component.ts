d5bf35cc923602e828d4a80cbce3030bf2309931
WanchalermDevs <42259969+WanchalermDevs@users.noreply.github.com>
2020-04-13 12:35:47 +0700

    setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}];
   title = 'lab01';
+
+  onServerAdded(serverData: { serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(brueprintData: { serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: brueprintData.serverName,
+      content: brueprintData.serverContent
+    });
+  }
 }
